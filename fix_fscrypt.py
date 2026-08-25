4cc8bab70cffbebe820666b07f52a06e2296176f
kenai <wzx080913@outlook.com>
2026-08-25 17:08:55 +0800

    fix: comment out fscrypt blocks with sed range

--- fix_fscrypt.py
+++ fix_fscrypt.py
@@ -1,40 +1,46 @@
-import re
+#!/usr/bin/env python3
+import os, re
 
+os.chdir(os.environ.get('WORKDIR', os.getcwd()))
+
+# Fix append.c and block.c - remove fscrypt function calls
 for fname in ['bootable/recovery/libtar/append.c', 'bootable/recovery/libtar/block.c']:
-    try:
-        with open(fname) as f:
-            content = f.read()
-    except FileNotFoundError:
+    if not os.path.exists(fname):
+        print(f'Skip {fname}')
         continue
-    
-    # Find the fscrypt block: from fscrypt_policy_get_struct to the matching closing brace
-    # Use a simpler approach: just comment out problematic lines
-    
-    # Comment out lines with incompatible function calls
-    lines = content.split('\n')
-    new_lines = []
+    with open(fname) as f:
+        c = f.read()
+    # Remove the if blocks containing fscrypt function calls
+    # Simple approach: comment out problematic lines
+    lines = c.split('\n')
+    new = []
+    in_skip = False
+    brace_count = 0
     for line in lines:
-        if any(fn in line for fn in [
+        if not in_skip and any(fn in line for fn in [
             'fscrypt_policy_get_struct', 'fscrypt_policy_size',
             'get_policy_size', 'get_policy_descriptor',
-            'lookup_ref_key(t->th_buf.fep', 'get_policy(',
-            'fscrypt_policy *fep', 'struct fscrypt_policy_v1'
+            'lookup_ref_key(t->th_buf.fep'
         ]):
-            new_lines.append('/* DISABLED: ' + line.strip() + ' */')
-        else:
-            new_lines.append(line)
-    
+            in_skip = True
+            brace_count = line.count('{') - line.count('}')
+            continue
+        if in_skip:
+            brace_count += line.count('{') - line.count('}')
+            if brace_count <= 0:
+                in_skip = False
+            continue
+        new.append(line)
     with open(fname, 'w') as f:
-        f.write('\n'.join(new_lines))
+        f.write('\n'.join(new))
     print(f'Patched {fname}')
 
 # Fix libtar.h
-try:
-    with open('bootable/recovery/libtar/libtar.h') as f:
-        content = f.read()
-    content = content.replace('fscrypt_policy  *fep;', 'struct fscrypt_policy_v1 *fep;')
-    with open('bootable/recovery/libtar/libtar.h', 'w') as f:
-        f.write(content)
-    print('Patched libtar.h')
-except:
-    pass
+fname = 'bootable/recovery/libtar/libtar.h'
+if os.path.exists(fname):
+    with open(fname) as f:
+        c = f.read()
+    c = c.replace('fscrypt_policy  *fep;', 'struct fscrypt_policy_v1 *fep;')
+    with open(fname, 'w') as f:
+        f.write(c)
+    print(f'Patched {fname}')
