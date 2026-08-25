0b9fa3b4704e49943b7def51463ad12b2026ea42
kenai <wzx080913@outlook.com>
2026-08-25 17:13:01 +0800

    fix: use standalone python patcher for libtar fscrypt

--- fix_fscrypt.py
+++ fix_fscrypt.py
@@ -1,46 +1,40 @@
 #!/usr/bin/env python3
-import os, re
+import os
 
 os.chdir(os.environ.get('WORKDIR', os.getcwd()))
 
-# Fix append.c and block.c - remove fscrypt function calls
 for fname in ['bootable/recovery/libtar/append.c', 'bootable/recovery/libtar/block.c']:
     if not os.path.exists(fname):
         print(f'Skip {fname}')
         continue
     with open(fname) as f:
-        c = f.read()
-    # Remove the if blocks containing fscrypt function calls
-    # Simple approach: comment out problematic lines
-    lines = c.split('\n')
-    new = []
-    in_skip = False
-    brace_count = 0
+        lines = f.readlines()
+    skip_until = 0
+    result = []
     for line in lines:
-        if not in_skip and any(fn in line for fn in [
-            'fscrypt_policy_get_struct', 'fscrypt_policy_size',
-            'get_policy_size', 'get_policy_descriptor',
-            'lookup_ref_key(t->th_buf.fep'
-        ]):
-            in_skip = True
-            brace_count = line.count('{') - line.count('}')
+        if skip_until > 0:
+            skip_until += line.count('{') - line.count('}')
+            if skip_until <= 0:
+                skip_until = 0
             continue
-        if in_skip:
-            brace_count += line.count('{') - line.count('}')
-            if brace_count <= 0:
-                in_skip = False
+        if any(fn in line for fn in ['fscrypt_policy_get_struct', 'fscrypt_policy_size',
+                'get_policy_size', 'get_policy_descriptor',
+                'lookup_ref_key(t->th_buf.fep']):
+            skip_until = line.count('{') - line.count('}')
+            if skip_until <= 0:
+                skip_until = 0
             continue
-        new.append(line)
+        result.append(line)
     with open(fname, 'w') as f:
-        f.write('\n'.join(new))
-    print(f'Patched {fname}')
+        f.writelines(result)
+    print(f'Fixed {fname}')
 
-# Fix libtar.h
-fname = 'bootable/recovery/libtar/libtar.h'
-if os.path.exists(fname):
-    with open(fname) as f:
+try:
+    with open('bootable/recovery/libtar/libtar.h') as f:
         c = f.read()
     c = c.replace('fscrypt_policy  *fep;', 'struct fscrypt_policy_v1 *fep;')
-    with open(fname, 'w') as f:
+    with open('bootable/recovery/libtar/libtar.h', 'w') as f:
         f.write(c)
-    print(f'Patched {fname}')
+    print('Fixed libtar.h')
+except:
+    pass
