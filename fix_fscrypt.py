1b74c0485c3e019135e8011daf6c08add01a5a6b
kenai <wzx080913@outlook.com>
2026-08-25 23:04:26 +0800

    fix: aggressive fscrypt patching for libtar

--- fix_fscrypt.py
+++ fix_fscrypt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Wrap fscrypt blocks with #if 0 instead of deleting lines."""
+"""Aggressively fix fscrypt by commenting out problematic blocks."""
 import os
 
 for fname in ['bootable/recovery/libtar/append.c', 'bootable/recovery/libtar/block.c']:
@@ -9,27 +9,34 @@ for fname in ['bootable/recovery/libtar/append.c', 'bootable/recovery/libtar/blo
     with open(fname) as f:
         lines = f.readlines()
     
+    # Keywords that indicate fscrypt-related problematic code
+    keywords = [
+        'fscrypt_policy_get_struct', 'fscrypt_policy_size',
+        'get_policy_size', 'get_policy_descriptor',
+        'lookup_ref_key(t->th_buf.fep',
+        't->th_buf.fep',
+    ]
+    
     result = []
-    skip_until = 0
-    skip_start = -1
+    in_skip = False
+    brace_depth = 0
     
     for i, line in enumerate(lines):
-        if skip_until > 0:
-            skip_until += line.count('{') - line.count('}')
-            if skip_until <= 0:
-                skip_until = 0
+        if in_skip:
+            brace_depth += line.count('{') - line.count('}')
+            if brace_depth <= 0:
+                in_skip = False
                 result.append('#endif /* fscrypt disabled */\n')
             continue
         
-        # Detect start of fscrypt block
-        if any(fn in line for fn in ['fscrypt_policy_get_struct', 'fscrypt_policy_size',
-                'get_policy_size', 'get_policy_descriptor',
-                'lookup_ref_key(t->th_buf.fep']):
-            skip_until = line.count('{') - line.count('}')
+        # Check if this line starts a problematic block
+        if any(kw in line for kw in keywords):
+            in_skip = True
+            brace_depth = line.count('{') - line.count('}')
             result.append('#if 0 /* fscrypt disabled for compatibility */\n')
             result.append(line)
-            if skip_until <= 0:
-                skip_until = 0
+            if brace_depth <= 0:
+                in_skip = False
                 result.append('#endif /* fscrypt disabled */\n')
             continue
         
@@ -39,7 +46,7 @@ for fname in ['bootable/recovery/libtar/append.c', 'bootable/recovery/libtar/blo
         f.writelines(result)
     print(f'Fixed {fname}')
 
-# Fix libtar.h struct tag
+# Fix libtar.h
 fname = 'bootable/recovery/libtar/libtar.h'
 if os.path.exists(fname):
     with open(fname) as f:
